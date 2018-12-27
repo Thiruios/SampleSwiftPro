9b3774ceddcaa7ef9dd4e85fc28f8a6c6473e8fb
rajthiru <rajthiru2012@gmail.com>
2018-12-27 15:20:51 +0530

    e

--- SampleSwiftPro/AppDelegate.swift
+++ SampleSwiftPro/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        print("Thiru")
+        print("Thiru2")
         
         
         // Override point for customization after application launch.
